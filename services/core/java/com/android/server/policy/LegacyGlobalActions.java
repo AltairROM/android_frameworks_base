abbf1328f05b38867039f611a98526687b99b5e1
Martin J. Pollard <martin0566@gmail.com>
2022-04-30 14:08:55 -0400

    Remove LineageOS bug reporting

--- services/core/java/com/android/server/policy/LegacyGlobalActions.java
+++ services/core/java/com/android/server/policy/LegacyGlobalActions.java
@@ -281,11 +281,13 @@ class LegacyGlobalActions implements DialogInterface.OnDismissListener, DialogIn
                 mItems.add(new PowerAction(mContext, mWindowManagerFuncs));
             } else if (GLOBAL_ACTION_KEY_AIRPLANE.equals(actionKey)) {
                 mItems.add(mAirplaneModeOn);
+/*
             } else if (GLOBAL_ACTION_KEY_BUGREPORT.equals(actionKey)) {
                 if (Settings.Global.getInt(mContext.getContentResolver(),
                         Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0 && isCurrentUserOwner()) {
                     mItems.add(new BugReportAction());
                 }
+*/
             } else if (GLOBAL_ACTION_KEY_SILENT.equals(actionKey)) {
                 if (mShowSilentToggle) {
                     mItems.add(mSilentModeAction);
