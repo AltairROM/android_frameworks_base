46b4b8ec6ca416233ead41599caa4681063abbf4
Martin J. Pollard <martin0566@gmail.com>
2022-05-10 15:51:54 -0400

    base: Show icon of package associated with Toast
    For all those times you have some random app or background service that posts
    a Toast and you have no idea who's posting it. This adds an icon badge to the
    top left corner of the Toast to show the app's icon the Toast belongs to.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7154,6 +7154,13 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        @Readable
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -18097,7 +18104,7 @@ public final class Settings {
          * Force LTE Carrier Aggregation setting per SIM slot.
          * Use FORCE_LTE_CA_0 for SIM slot 0 (first SIM)
          * Use FORCE_LTE_CA_1 for SIM slot 1 (second SIM)
-         * 
+         *
          * Type: int (0 = disabled, 1 = enabled)
          * @hide
          */
