50e7c6ef6612d3878638f24da21b60f7091771fe
Pranav Vashi <neobuddy89@gmail.com>
2024-01-07 11:39:10 +0530

    crdroid: Safeguard against stub apks when checking if package enabled
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/custom/Utils.java
+++ core/java/com/android/internal/util/custom/Utils.java
@@ -42,7 +42,7 @@ public class Utils {
         if (packageName != null) {
             try {
                 PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
-                if (!pi.applicationInfo.enabled && !ignoreState) {
+                if ((!pi.applicationInfo.enabled || !pi.applicationInfo.isProduct()) && !ignoreState) {
                     return false;
                 }
             } catch (PackageManager.NameNotFoundException e) {
@@ -59,7 +59,7 @@ public class Utils {
     public static boolean isPackageEnabled(Context context, String packageName) {
         try {
             PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
-            return pi.applicationInfo.enabled;
+            return pi.applicationInfo.enabled && pi.applicationInfo.isProduct();
         } catch (PackageManager.NameNotFoundException notFound) {
             return false;
         }
