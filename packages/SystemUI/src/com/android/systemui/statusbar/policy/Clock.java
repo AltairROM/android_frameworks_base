8eeaa798b8b0deb40d60ab6e6d9ef882db50d1aa
Pranav Vashi <neobuddy89@gmail.com>
2024-01-26 23:29:54 +0530

    Clock: Use executor for tuner settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -261,8 +261,10 @@ public class Clock extends TextView implements
 
         // Make sure we update to the current time
         updateShowSeconds();
-        updateClock();
-        updateClockVisibility();
+        mContext.getMainExecutor().execute(() -> {
+            updateClock();
+            updateClockVisibility();
+        });
     }
 
     @Override
@@ -466,6 +468,10 @@ public class Clock extends TextView implements
             default:
                 break;
         }
+        mContext.getMainExecutor().execute(() -> {
+            updateClock(true);
+            updateClockVisibility();
+        });
     }
 
     @Override
