6dd5788b4565a88a406add0f9ffdaba4f96ba4d8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-18 20:44:16 +0800

    SystemUI: Add pulse visualizer
    neobuddy89:
    * Add Settings keys in Settings.java
    * Fix for multi-user usage
    * Disable rounded corners by default
    
    Change-Id: Ic7b2353d9214c3ecd3b9438e0be0222e509b52de
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -27,6 +27,7 @@ import com.android.systemui.dump.DumpManager;
 import com.android.systemui.keyguard.KeyguardSliceProvider;
 import com.android.systemui.media.MediaViewController;
 import com.android.systemui.people.PeopleProvider;
+import com.android.systemui.pulse.PulseViewController;
 import com.android.systemui.startable.Dependencies;
 import com.android.systemui.statusbar.NotificationInsetsModule;
 import com.android.systemui.statusbar.NTForbiddenSwipeDownQSController;
@@ -201,4 +202,7 @@ public interface SysUIComponent {
 
     @SysUISingleton
     MediaViewController mediaViewController();
+
+    @SysUISingleton
+    PulseViewController pulseViewController();
 }
