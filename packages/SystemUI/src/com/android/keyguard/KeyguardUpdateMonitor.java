71c3f4260f07196dce59c2b44b6810b16bde73b2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-22 14:40:52 +0800

    SystemUI: Add ScrimUtils helper [SQUASHED]
    [neobuddy89: Squashed all fixes by same author.]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -168,6 +168,7 @@ import com.android.systemui.telephony.TelephonyListenerManager;
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor;
 import com.android.systemui.util.Assert;
 import com.android.systemui.util.kotlin.JavaAdapter;
+import com.android.systemui.util.ScrimUtils;
 
 import dalvik.annotation.optimization.NeverCompile;
 
@@ -2078,6 +2079,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
             }
         }
         Trace.endSection();
+
+        ScrimUtils.get().onStartedWakingUp();
     }
 
     protected void handleStartedGoingToSleep(int arg1) {
@@ -2114,6 +2117,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
     private void handleScreenTurnedOff() {
         Assert.isMainThread();
         mHardwareFingerprintUnavailableRetryCount = 0;
+        ScrimUtils.get().onScreenTurnedOff();
     }
 
     private void handleDreamingStateChanged(int dreamStart) {
