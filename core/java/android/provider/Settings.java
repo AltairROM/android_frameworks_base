2a8ac6a2e7a8e7f1cd9c0b3bb7e8e4ac7917225f
Pranav Vashi <neobuddy89@gmail.com>
2025-10-17 16:42:24 +0530

    SystemUI: Add toggle for doze wallpaper
    * Make AOSP switch DOZE_ALWAYS_ON_WALLPAPER_ENABLED functional and let user select.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11089,6 +11089,7 @@ public final class Settings {
          *
          * @hide
          */
+        @Readable
         public static final String DOZE_ALWAYS_ON_WALLPAPER_ENABLED =
                 "doze_always_on_wallpaper_enabled";
 
