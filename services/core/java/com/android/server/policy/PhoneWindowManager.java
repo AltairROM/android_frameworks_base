095b6e4f90e48979174436fb85318d4fa40f8d00
iusmac <iusico.maxim@libero.it>
2025-11-10 20:36:34 +0100

    PhoneWindowManager: Fix duplicate handling of App switch key press event
    Issue:
        App switch key press events are handled twice in the current
        codebase. This results in the Recent apps screen being toggled
        regardless of what action was selected by user when this button is
        short/long/double pressed. For example:
        - When Action.NOTHING is selected, Recent apps still triggers.
        - When Action.TORCH is selected, both Recent apps and the torch
          toggle.
        - When Action.APP_SWITCH is selected, Recent apps toggles twice,
          making it unusable.
    
    Analysis:
        InputManager routes this key to KeyGestureController, which triggers
        the KeyGestureEventHandler listener in PhoneWindowManager, where we
        process it without considering the user's preference, then we
        handle it one more time in our dedicated rule for this key added in
        commit 6c82230544ccac49e510fa669dfde3157762ca2b ("base: Use SingleKeyRule for app switch long press"),
        which *does* respect user preference.
    
    Solution:
        Listening to KeyGestureController is not needed since the
        SingleKeyRule already handles all cases, including toggling Recent
        apps when applicable.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1808,7 +1808,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void appSwitchPress(int count) {
-        if (count == 1 && mAppSwitchPressAction != Action.NOTHING) {
+        if (count == 1 && mAppSwitchPressAction != Action.NOTHING && !keyguardOn()) {
             if (mAppSwitchPressAction != Action.APP_SWITCH) {
                 cancelPreloadRecentApps();
             }
@@ -1818,7 +1818,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     KeyEvent.FLAG_FROM_SYSTEM, InputDevice.SOURCE_KEYBOARD);
 
             performKeyAction(mAppSwitchPressAction, event);
-        } else if (count == 2 && mAppSwitchDoubleTapAction != Action.NOTHING) {
+        } else if (count == 2 && mAppSwitchDoubleTapAction != Action.NOTHING && !keyguardOn()) {
             if (mAppSwitchDoubleTapAction != Action.APP_SWITCH) {
                 cancelPreloadRecentApps();
             }
@@ -4772,7 +4772,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         List<Integer> supportedGestures = new ArrayList<>(List.of(
                 KeyGestureEvent.KEY_GESTURE_TYPE_RECENT_APPS,
-                KeyGestureEvent.KEY_GESTURE_TYPE_APP_SWITCH,
                 KeyGestureEvent.KEY_GESTURE_TYPE_LAUNCH_ASSISTANT,
                 KeyGestureEvent.KEY_GESTURE_TYPE_LAUNCH_VOICE_ASSISTANT,
                 KeyGestureEvent.KEY_GESTURE_TYPE_HOME,
@@ -4831,15 +4830,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyGestureEvent.KEY_GESTURE_TYPE_RECENT_APPS:
                 if (complete) {
                     showRecentApps(false);
-                }
-                break;
-            case KeyGestureEvent.KEY_GESTURE_TYPE_APP_SWITCH:
-                if (!keyguardOn) {
-                    if (start) {
-                        preloadRecentApps();
-                    } else if (complete) {
-                        toggleRecentApps();
-                    }
                 }
                 break;
             case KeyGestureEvent.KEY_GESTURE_TYPE_LAUNCH_ASSISTANT:
