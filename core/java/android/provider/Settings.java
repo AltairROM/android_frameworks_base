TurboROM-Legacy/frameworks_base

SystemUI: Forward-port notification counters
 * Port from CM11

Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

core/java/android/provider/Settings.java
@@ -4201,6 +4201,12 @@ public final class Settings {
         public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_regex_enabled";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

AltairROM/android_frameworks_base

Squashed:
    From: d34d <clark@cyngn.com>
    Date: Mon, 13 Feb 2017 01:12:57 -0500
    Subject: [PATCH 292/732] SystemUI: Show infinity for large notification counts
    This patch adds a new string to SystemUI for displaying the infinity
    symbol when a notification has a count larger than 999.  This only
    affects the notification icon in the status bar and does not change
    the text displayed in the actual notification.
    These changes were manually cherry-picked from cm-13.0.
    Change-Id: I3481aac13cd90ee20ed48194df3ec789c16c8b4b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    From: jhonboy121 <alfredmathew05@gmail.com>
    Date: Sun, 2 Jan 2022 18:09:39 +0530
    Subject: SystemUI: Refactor notification counter
    * Update icons from NotificationIconAreaController when settings change and set initial state from StatusBarIconController
    * Also use value of config_statusBarShowNumber as the default
    [neobuddy89: Add required NPE checks to properly apply settings on boot.]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    From: Fran Palmero <sweetydesigns@gmail.com>
    Date: Thu, 21 Jul 2022 05:05:10 +0200
    Subject: SystemUI: Update notification count overlay
    * Updated to A12 style
    * Overlay picked from figma.com/community/plugin/1088610476491668236 edited with GIMP 2.10.18
[@neobuddy89: Updated and adapted for A14 QPR1, Restored and reworked for A15 since Google deleted it]
Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/provider/Settings.java
@@ -6618,6 +6618,12 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *